c0d469f0de17a81d09ca2c259bdcc4f04f43ec44
sannawag <scwager@indiana.edu>
2016-09-17 21:35:29 -0400

    working version where f(n) = the number of tables

--- problem3/problem3.py
+++ problem3/problem3.py
@@ -1,5 +1,7 @@
+import copy
 import sys
 from heapq import *
+
 
 #returns friend pairs and a list containing all the names
 #pairs - assumed to be sparce data -  are stored in an adjacency list.
@@ -18,45 +20,48 @@ def read_friends(filename):
                pairs[i].add(n[0])
     return pairs, list(name_set)
 
-def seating_successors( pairs, unseated, table_size, curr ):
+
+#def seating_successors( pairs, unseated, table_size, curr ):
+def seating_successors( pairs, person, table_size, curr ): 
     successors = []
-    person = unseated.pop()
-    found_spot = False
-    for table in curr[1]:
-        print "table", table
-        if found_spot is True:
-            break
-        if len( table ) >= table_size:
-            continue
-        table.append( person )
-        found_spot = True 
-    if found_spot is False:
-        curr[1].append( [person] )
     print "curr", curr
-    successors.append(curr)
+    for (i, table) in enumerate(curr[1]):
+        if len( table ) >= table_size or len(table & pairs[person]) > 0:
+            continue
+        new = copy.deepcopy(curr)
+        new[1][i].add( person )
+        successors.append( new )
+    curr[1].append( {person} )
+    successors.append( (curr[0] + 1, curr[1]) ) #update the table count
     return successors
-    
-    
+
+
+def count_seated( table_config ):
+    return sum( [len(x) for x in table_config[1]] )
+
+
+def is_solution( table_config, person_count ):
+    return count_seated( table_config ) == person_count
+
 
 def assign_awkward_seating(pairs, name_list, table_size):
     fringe = []
-    heappush( fringe, (1, [ [name_list.pop()] ]) ) #priority queue for cost and tables so far
-    while (name_list):
-        print "fringe", fringe
+    print "pairs", pairs
+    person_count = len(name_list)
+#    heappush( fringe, (1, [ {name_list.pop()} ]) )
+    heappush( fringe, (1, [ {name_list[0]} ]) ) #priority queue for cost and tables so far
+    while True:
         curr = heappop( fringe )
-        if not name_list:
+        if is_solution(curr, person_count):
             print "solution", curr
             return curr
-        for s in seating_successors( pairs, name_list, table_size, curr):
+        for s in seating_successors( pairs, name_list[ count_seated(curr) ], table_size, curr):
             heappush( fringe, s )
     print "The program did not find a solution and probably has a bug."
-        
-        
       
+
 if "__main__" == __name__:
-    friends_file = sys.argv[1]
-    table_size = sys.argv[2]
-    
-    pairs, namelist = read_friends(friends_file)
-    #solve the seating assignments!
-    assign_awkward_seating(pairs, namelist, table_size)
+    friends_file = str(sys.argv[1])
+    table_size = int(sys.argv[2])    
+    pairs, namelist = read_friends(friends_file) #read in the list of names and friendship graph!
+    assign_awkward_seating(pairs, namelist, table_size) #solve the assignments!
