0b0b5059fdaa77ac09ca8e3c53f8efb9a5275ada
Saurav Maheshwary <samahesh@silo.soic.indiana.edu>
2016-09-19 01:52:42 -0400

    BFS, Graceful handling of missing data, Displaying Route as asked

--- problem1/solution.py
+++ problem1/solution.py
@@ -1,5 +1,9 @@
+#!/usr/local/bin/python3
+from collections import deque
 import sys
+import time
 
+finalOutput = deque()
 '''
 Represents all properties of a city - its name, latitude, longitude and its neighbors.
 neighbors is a dictionary with name of connected city as key and a tuple (length, speed, highway name) as value.
@@ -10,12 +14,16 @@ class City:
 		self.latitude = latitude
 		self.longitude = longitude
 		self.neighbors = {}
+		self.visited = False #False = unvisited, True = visited
 
 	def addNeighbor(self, neighborName, length, speed, nameOfHighway):
-		self.neighbors[neighborName] = (length, speed, nameOfHighway)
+		distance = float(length)
+		speedLimit = float(speed)
+		time = round(distance/speedLimit, 2) #round(distance/70.0, 2) if speed=="0" else round(distance/speedLimit, 2)
+		self.neighbors[neighborName] = (distance, speedLimit, time, nameOfHighway)
 	
 	def getAllNeighboringCities(self):
-		return self.neighbors.keys()
+		return self.neighbors.items()
 
 	def getWeight(self, neighborName):
 		return self.neighbors[neighborName]
@@ -27,49 +35,146 @@ nodeList is a dictionary which stores name of the city as key and the City Objec
 class RoadNetwork:
 	def __init__(self):
 		self.nodeList = {}
+		self.followThisRoute = {} #Stores Parent of every node
+		self.totalMiles = 0.0
+		self.totalHours = 0.0
 
 	def readCityGpsFile(self):
-		cities = open("city-gps.txt", "r").read().split("\n")
-		for i in range(len(cities) - 1):
-			city = cities[i].split()
-			node = City(city[0], city[1], city[2])
-			#print("City: " + node.cityName + " Latitude: " + node.latitude + " Longitude: " + node.longitude)
+		citiesFile = open("city-gps.txt", "r")
+		
+		for line in citiesFile:
+			city = line.split()
+			node = City(*city)
 			self.nodeList[city[0]] = node
-			#print("City: " + city[0] + " Latitude: " + city[1] + " Longitude: " + city[2])
-		#for node, nodeObj in self.nodeList.items():	
-			#print(node + ": " + nodeObj.cityName)
-	
+		
+		#self.nodeList = {line.split()[0]: City(*line.split()) for line in citiesFile}
+		#citiesFile.close()
+
 	def readRoadSegmentsFile(self):
-		segments =  open("road-segments.txt", "r").read().split("\n")
-		for i in range(len(segments) - 1):
-			segment = segments[i].split()
-			#print(segment)
-			#Handling missing speed values in file
-			if (len(segment) == 5):
-				firstCity, secondCity, length, speed, highwayName = segment[0], segment[1], segment[2], segment[3], segment[4]
+		segmentsFile =  open("road-segments.txt", "r")
+		for line in segmentsFile:
+			segment = line.split()
+			#Handling missing speed/length values in file - Will ignore that record.
+			if("0" in segment or len(segment) is 4):#segment[2] == "0" or segment[3] == "0" or len(segment) == 4:
+				continue
+			firstCity, secondCity, length, speed, highwayName = segment
+			'''if (len(segment) == 5):
+				firstCity, secondCity, length, speed, highwayName = segment
 			elif (len(segment) == 4):
-				firstCity, secondCity, length, highwayName = segment[0], segment[1], segment[2], segment[3]
+				firstCity, secondCity, length, highwayName = segment
+			'''
+			#Handling Junction names - these names are not present in city-gps.txt
 			if(firstCity not in self.nodeList):
 				self.nodeList[firstCity] = City(firstCity, None, None)
 			if(secondCity not in self.nodeList):
 				self.nodeList[secondCity] = City(secondCity, None, None)
 			self.nodeList[firstCity].addNeighbor(secondCity, length, speed, highwayName)
 			self.nodeList[secondCity].addNeighbor(firstCity, length, speed, highwayName)
-		self.displayGraph()
+		#segmentsFile.close()
+		#self.displayGraph()
 
 	def displayGraph(self):
 		for node, nodeObj in self.nodeList.items():
-			#if(node.startswith("Y_City")):
-				print(node + ": " + str(nodeObj.getAllNeighboringCities()))
-	
-def takeInput():
-	enteredValue = sys.argv
-	startCity = enteredValue[1]
-	endCity = enteredValue[2]
-	routingOption = enteredValue[3]
-	#print(enteredValue)
+			print(node + ": " + str(nodeObj.getAllNeighboringCities())+"\n")
 
-takeInput()
-graph = RoadNetwork()
-graph.readCityGpsFile()
-graph.readRoadSegmentsFile()
+	def findRoute(self, startCityName, endCityName, routingOption, routingAlgorithm):
+		if(routingAlgorithm == "bfs"):
+			return self.bfs(startCityName, endCityName)
+		elif(routingAlgorithm == "dfs"):
+			return self.dfs(startCityName, endCityName)
+		elif(routingAlgorithm == "ids"):
+			return self.ids(startCityName, endCityName)
+		else:
+			return self.astar(startCityName, endCityName, routingOption)
+
+	#Returns True if path is found, else False
+	def bfs(self, startCityName, endCityName):
+		childParentDict = {}
+		startCityObj = self.nodeList[startCityName]
+		q = deque([startCityObj])
+		while(q):
+			currentCityObj = q.popleft()
+			if(currentCityObj.cityName == endCityName):
+				self.createRouteFromDict(childParentDict, currentCityObj)
+				return True
+			currentCityObj.visited = True
+			for neighboringCityName, edgeWeight in currentCityObj.getAllNeighboringCities():
+				neighboringCityObj = self.nodeList[neighboringCityName]
+				if(not neighboringCityObj.visited):
+					q.append(neighboringCityObj)
+					childParentDict[neighboringCityObj] = currentCityObj
+		print("Path not found! :(")
+		return False
+
+	def createRouteFromDict(self, childParentDict, endCityObj):
+		while(endCityObj in childParentDict):
+			parentNode = childParentDict[endCityObj]
+			self.followThisRoute[parentNode] = endCityObj
+			endCityObj = parentNode
+
+	'''
+	Displays the route, total miles and total time in human readable format
+	'''
+	def displayRoute(self, startCityObj):
+		
+		print("\nMove from".ljust(40,' ') + "To".ljust(40, ' ') + "On Road".ljust(16,' ') + "Total Miles".ljust(20, ' ') + "Miles/Hr".ljust(13, ' ') + "Total Hrs." + "\n" + "-"*150)
+		while(startCityObj in self.followThisRoute):
+			neighboringCityObj = self.followThisRoute[startCityObj]
+			distance, speed, time, highwayName = startCityObj.neighbors[neighboringCityObj.cityName]
+			self.totalMiles += distance
+			self.totalHours += time
+			finalOutput.append(startCityObj.cityName)
+			print(startCityObj.cityName.ljust(40, ' ') + neighboringCityObj.cityName.ljust(40, ' ') + highwayName.ljust(16, ' ') + str(distance).ljust(20, ' ') + str(speed).ljust(15, ' ') + str(time))
+			startCityObj = neighboringCityObj
+		print("\nTotal Travel Miles: " + str(self.totalMiles) + "\nTotal Travel Hours: " + str(self.totalHours))
+		finalOutput.appendleft(str(self.totalHours))
+		finalOutput.appendleft(str(self.totalMiles))
+
+	def dfs(self, startCityName, endCityName):
+		return
+
+	def ids(self, startCityName, endCityName):
+		return
+	
+	def astar(self, startCityName, endCityName, routingOption):
+		return
+
+def validateRoutingOptions(routingAlgorithm, routingOption):
+	if(routingOption not in ("segments", "distance", "time", "scenic")):
+		print("Incorrect routing option entered! Please enter one of segments, distance, time or scenic.")
+		sys.exit(0)
+	elif(routingAlgorithm not in ("bfs", "dfs", "ids", "astar")):
+		print("Incorrect routing algorithm entered! Please enter one of bfs, dfs, ids or astar.")
+		sys.exit(0)
+
+def validateCities(graph, startCity, endCity):
+	if(startCity not in graph.nodeList or endCity not in graph.nodeList):
+		print("Invalid City name entered!")
+		sys.exit(0)
+
+def main():
+	enteredValue = sys.argv
+	startCityName = enteredValue[1]
+	endCityName = enteredValue[2]
+	routingOption = enteredValue[3]
+	routingAlgorithm = enteredValue[4]
+	validateRoutingOptions(routingAlgorithm, routingOption)
+	
+	startTime = time.time()
+	graph = RoadNetwork()
+	graph.readCityGpsFile()
+	graph.readRoadSegmentsFile()
+	endTime = time.time()
+	validateCities(graph, startCityName, endCityName)
+	
+	#graph.displayGraph()
+	print("Created road network in: " + str((endTime - startTime)/60) + " min.")
+	startTime = time.time()
+	foundRoute = graph.findRoute(startCityName, endCityName, routingOption, routingAlgorithm)
+	endTime = time.time()
+	if(foundRoute):
+		graph.displayRoute(graph.nodeList[startCityName])
+		print("Found route in: " + str((endTime - startTime)/60) + " min.")
+	print("\n" + " ".join(finalOutput))
+if __name__ == "__main__":
+	main()
