9ca9d16541d9715429b310750f645f5f109ac057
sannawag <scwager@indiana.edu>
2016-09-25 13:40:59 -0400

    solves a simple board using A*

--- solver15.py
+++ solver15.py
@@ -5,6 +5,7 @@ from heapq import *
 import sys
 
 N = 16
+solution = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0]
 
 def read_board(filename):
     board = []
@@ -26,34 +27,12 @@ def check_parity(board):
     count = 0
     for i in range(N):
         count += i//4 + 1 if board[i] == 0 else len( [ x for x in board[i:N] if x > 0 and x < board[i] ] )
-        print "curr tile", board[i], "count", count
-    print "is the board even? ", count % 2 == 0
     return count % 2 == 0
 
-def heuristic_misplacetiles(initial_state):
-    while len(fringe) > 0:
-        temp = fringe.pop()
-        mis_pl = 0
-        for i in range(0, 4):
-            for j in range(0, 4):
-                if goal_state[i][j] != temp[i][j]:
-                    mis_pl = mis_pl + 1
-        heappush(h, (mis_pl, temp))
-        return (heappop(h))
-        
-def mh(state, a): #manhattan heuristic
-    for i in range(0, 4):
-        for j in range(0, 4):
-            value = state[i][j]
-            goal_index = goal_dict[value]
-            a = a + abs(i // 4 - goal_index[0]) + abs(j - goal_index[1])
-    return a 
-    
-    
-def successor_mh(curr, goal):
+def successor_mh(curr):
     successor = []
     pos = curr[1]
-    depth = curr[0]
+    depth = curr[0] + 1
     #case 1: left moves are allowed
     if pos in set( range(N) ) - set( [4, 8] ): 
         if pos in [0, 12]:
@@ -78,30 +57,28 @@ def successor_mh(curr, goal):
             successor.append( (depth, pos-12) )
         else:
             successor.append( (depth, pos+4) )
-    print "pos", pos, "goal", goal, "successor", successor
     return successor
 
 def find_shortest_path_mh(pos, goal):
-    print "pos", pos
-    visited = set(pos)
+    visited = set([pos])
     fringe = [ (0, pos) ]
     while True:
         curr = heappop(fringe)
-        visited |= curr[1] #add position to explored
+        visited.add( curr[1] ) #add position to explored
         if curr[1] == goal:
             return curr[0] #the minimum distance
         for s in successor_mh(curr):
             if s[1] not in visited and s not in fringe:
                 heappush(fringe, s) 
-    print "there was a bug"
     
 def mh(board): #manhattan heuristic taking into account option of jumping between corners
-    print "board", board
+    #print "calculating manhattan distance for board: ", board
     total_moves = 0
     for i in range(N):
         if board[i] == 0: #empty tile
             continue
         total_moves += find_shortest_path_mh(i, board[i] - 1) #goal index is 1 less than the number on the tile
+    #print "total manhattan distance", total_moves
     return total_moves
 
 # function for generating various states of the puzzle
@@ -145,19 +122,18 @@ def successor(a):
             b[pos], b[pos+4] = b[pos+4], b[pos]
         h = mh(b)
         successors.append( (g+h, b, g) ) 
-    print "successors:"
-    for s in successors: print_board(s)
     return successors
 
 
 def solve(initial_state):
     fringe = [ (0, initial_state, 0) ] #evaluation function (f = g+h); board; depth of state
-#    while len(fringe) > 0: #comment this out to actually run the program
-    for i in range(3):
+    while len(fringe) > 0: #comment this out to actually run the program
         curr = heappop(fringe)
-        print "curr", "f = ", curr[0], "depth = ", curr[2]
+        print "current state:", "f = ", curr[0], "depth = ", curr[2], "board:\n"
         print_board(curr)
-        #check is this the solution
+        if curr[1] == solution:
+            print "solution found. now need to backtrack."
+            return True
         for s in successor(curr):
             fringe.append(s)
     return False
